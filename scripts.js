574c9a6654a74a4b2670b23e588ba4a326d7d4fc
craigreedwilliams <craig.williams001@student.swic.edu>
2019-10-04 11:10:52 -0700

    add emmit snip to index. add functionality to scripts.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,35 @@
-// Enter a description of this program here.
+// calculator app
 
 /**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
+ *
  */
+
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function remainder(a, b) {
+  return a % b;
+}
+
+const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("num-2");
+const select = document.querySelector("select");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(num1.value, num2.value, select.value);
+});
