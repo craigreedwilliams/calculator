a6494f6d7058b39b245665bda894bf48184b400d
craigreedwilliams <craig.williams001@student.swic.edu>
2019-10-13 20:13:11 -0500

    Use BRACKET NOTATION to INVOKE APP METHOD.

--- scripts.js
+++ scripts.js
@@ -32,8 +32,9 @@ const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  console.log(num1.value, num2.value, select.value);
+  output.textContent = calculator[select.value](num1.value, num2.value);
 });
